8a758f080e1e3dc0f8011e25b968d4f5f80faabc
Lukas Müller <mail@lukasmueller.de>
2019-07-04 10:32:42 +0200

    Fix compile.sh and manifest.yml
    This commit applies small fixes to those files and re-packages the
    .jar-file.

--- assets/sample-apps/java/compile.sh
+++ assets/sample-apps/java/compile.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if ! javac -target 8 -source 8 App.java; then
-    echo "An error occured while compiling"
+    echo "An error occurred while compiling"
     exit 1
 fi
 
 if ! jar cfe App.jar App App.class; then
-    echo "An error occured while compiling"
+    echo "An error occurred while packaging"
     exit 1
 fi
 
