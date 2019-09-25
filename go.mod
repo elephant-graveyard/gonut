d9c827c1a31131b5fbd10206c0029e2ab78f7953
Lukas Müller <mail@lukasmueller.de>
2019-09-25 16:54:22 +0200

    Add update notification to version command
    In case the installed of gonut is outdated, it shows
    a notification on the `version` command with the respective
    command for installing the latest release.

--- go.mod
+++ go.mod
@@ -8,11 +8,15 @@ require (
 	github.com/gonvenience/term v1.0.0
 	github.com/gonvenience/text v1.0.2
 	github.com/gonvenience/wait v1.0.2
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/hashicorp/go-version v1.2.0 // indirect
 	github.com/homeport/pina-golada v1.4.0
 	github.com/lucasb-eyer/go-colorful v1.0.2
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/onsi/ginkgo v1.9.0
 	github.com/onsi/gomega v1.6.0
 	github.com/spf13/cobra v0.0.5
+	github.com/tcnksm/go-latest v0.0.0-20170313132115-e3007ae9052e
 	gopkg.in/yaml.v2 v2.2.2
 )
---
+++ internal/gonut/cf/update.go
@@ -0,0 +1,81 @@
+// Copyright © 2019 The Homeport Team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cf
+
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/gonvenience/bunt"
+	"github.com/lucasb-eyer/go-colorful"
+
+	"github.com/gonvenience/neat"
+	"github.com/tcnksm/go-latest"
+)
+
+var version string
+var goos string
+
+// NotifyLatestRelease
+func NotifyLatestRelease() error {
+	githubTag := &latest.GithubTag{
+		Owner:      "homeport",
+		Repository: "gonut",
+	}
+
+	if res, err := latest.Check(githubTag, version); err == nil && res.Outdated {
+		updateCommand := ""
+		switch goos {
+		case "darwin":
+			updateCommand = "brew install homeport/tap/gonut"
+		default:
+			updateCommand = "curl -fsL http://ibm.biz/Bd2t2v | bash"
+		}
+
+		headline := "New version of gonut available!"
+		table, _ := neat.Table(releaseNotificationTable(version, res.Current))
+		content := bunt.Sprintf("%s\nRun `YellowGreen{%s}` to update!",
+			table,
+			updateCommand,
+		)
+
+		gonutBlue, _ := colorful.MakeColor(color.RGBA{77, 173, 233, 255})
+		formatted := neat.ContentBox(headline, content, neat.HeadlineColor(gonutBlue))
+
+		fmt.Printf("\n%s", formatted)
+	}
+	return nil
+}
+
+func releaseNotificationTable(installed string, current string) [][]string {
+	rows := [][]string{
+		[]string{
+			bunt.Sprintf("*Release:*"),
+			bunt.Sprintf("Crimson{%s} → YellowGreen{%s}", current),
+		},
+		[]string{
+			bunt.Sprintf("*Changelog:*"),
+			bunt.Sprintf("CornflowerBlue{~https://github.com/homeport/gonut/releases/tag/v%s~}", current),
+		},
+	}
+
+	return rows
+}
